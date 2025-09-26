08411a88654917f0555f242b226f9b3177d9de99
Roman-PRV <prokopenkorv@gmail.com>
2025-09-26 21:41:39 +0200

    feat: add icons processing ss-05

--- laravel/app/Models/Game.php
+++ laravel/app/Models/Game.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Game extends Model
 {
     use HasFactory;
+
+    public function getIconUrlAttribute(): string
+    {
+        $raw = $this->attributes['icon_url'] ?? '';
+
+        if (is_string($raw) && $raw !== '') {
+            return url(Storage::url($raw));
+        }
+
+        return url(Storage::url('icons/default-icon.png'));
+    }
 }
