59ae6d8b3f33f535e755a811061480a51b7dd191
Roman-PRV <prokopenkorv@gmail.com>
2025-10-24 22:02:48 +0200

    refactor: improve ConfigHelper logic ss-015

--- laravel/app/Helpers/ConfigHelper.php
+++ laravel/app/Helpers/ConfigHelper.php
@@ -6,18 +6,14 @@ class ConfigHelper
 {
     public function getString(string $key, string $default = ''): string
     {
-        $value = config($key, $default);
-
-        if (is_string($value) && $value !== '') {
-            return $value;
-        }
+        $value = config($key);
 
         if (is_null($value)) {
             return $default;
         }
 
-        if (is_scalar($value)) {
-            return (string) $value;
+        if (is_string($value)) {
+            return $value;
         }
 
         return $default;
