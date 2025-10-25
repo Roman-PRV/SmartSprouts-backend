dfa82cc882eebdb7e0a23f597ea981b94ba95e77
Roman Prokopenko <149939372+Roman-PRV@users.noreply.github.com>
2025-10-25 17:51:27 +0200

    fix: missing icon ss-015 (#19)
    * fix: fix errors in swagger template ss-012
    
    * fix: fix default icon ss-015
    
    * feat: create config helper and config/games ss-015
    
    * refactor: improve ConfigHelper logic ss-015
    
    * chore: fix docker file path ss-015
    
    * refactor: improve Game model code ss-015
    
    * refactor: make the ConfigHelper method static ss-015
    
    * refactor: remove registration of HelpersServiceProvider ss-015
    
    * fix: repair docker config ss-015
    
    * fix: repair docker config ss-015

---
+++ laravel/app/Helpers/ConfigHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+class ConfigHelper
+{
+    public static function getString(string $key, string $default = ''): string
+    {
+        $value = config($key);
+
+        if (is_null($value)) {
+            return $default;
+        }
+
+        if (is_string($value)) {
+            return $value;
+        }
+
+        return $default;
+    }
+}
