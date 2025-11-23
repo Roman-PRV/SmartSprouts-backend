ccd790f0950a12aecdec4badd9b4c04ea3d87659
Roman-PRV <prokopenkorv@gmail.com>
2025-11-23 18:48:36 +0100

    feat: create TrueFalseImageService ss-024

---
+++ laravel/app/Games/TrueFalseImage/Services/TrueFalseImageService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Games\TrueFalseImage\Services;
+
+use App\Contracts\GameServiceInterface;
+use App\Exceptions\LevelsTableMissingException;
+use App\Games\TrueFalseImage\Models\TrueFalseImageLevel;
+use App\Games\TrueFalseImage\Models\TrueFalseImageStatement;
+use App\Models\Level;
+use Illuminate\Database\Eloquent\Collection;
+use InvalidArgumentException;
+
+class TrueFalseImageService implements GameServiceInterface
+{
+    /**
+     * Fetch all levels for the game (no statements attached). Throws LevelsTableMissingException when table is absent.
+     *
+     *
+     * @throws LevelsTableMissingException
+     * @throws InvalidArgumentException
+     */
+    public function fetchAllLevels(): Collection
+    {
+        return TrueFalseImageLevel::all();
+    }
+
+    /**
+     * Fetch a Level row by id.
+     *
+     * @throws LevelsTableMissingException
+     * @throws InvalidArgumentException
+     */
+    public function fetchLevel(int $levelId): ?Level
+    {
+        return TrueFalseImageLevel::find($levelId);
+    }
+
+    /**
+     * Fetch statements for a given level id.
+     *
+     *
+     * @throws InvalidArgumentException
+     */
+    public function fetchDataForLevel(int $levelId): Collection
+    {
+        return TrueFalseImageStatement::where('level_id', $levelId)->get();
+    }
+}
