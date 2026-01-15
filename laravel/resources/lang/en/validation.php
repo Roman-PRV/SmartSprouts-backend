cd53d9d426abecac8b26e2f4822bacc60008f10a
Roman-PRV <prokopenkorv@gmail.com>
2026-01-15 15:17:24 +0100

    refactor: reconcile validation message files ss-037

--- laravel/resources/lang/en/validation.php
+++ laravel/resources/lang/en/validation.php
@@ -14,21 +14,56 @@ return [
     */
 
     'failed_message' => 'Validation failed',
-    'email' => 'The :attribute field must be a valid email address.',
-    'required' => 'The :attribute field is required.',
-    'string' => 'The :attribute field must be a string.',
-    'min' => [
-        'numeric' => 'The :attribute field must be at least :min.',
-        'file' => 'The :attribute field must be at least :min kilobytes.',
-        'string' => 'The :attribute field must be at least :min characters.',
-        'array' => 'The :attribute field must have at least :min items.',
+    'active_url' => 'The :attribute field must be a valid URL.',
+    'array' => 'The :attribute field must be an array.',
+    'ascii' => 'The :attribute field must only contain single-byte alphanumeric characters and symbols.',
+    'before' => 'The :attribute field must be a date before :date.',
+    'before_or_equal' => 'The :attribute field must be a date before or equal to :date.',
+    'between' => [
+        'array' => 'The :attribute field must have between :min and :max items.',
+        'file' => 'The :attribute field must be between :min and :max kilobytes.',
+        'numeric' => 'The :attribute field must be between :min and :max.',
+        'string' => 'The :attribute field must be between :min and :max characters.',
     ],
+    'boolean' => 'The :attribute field must be true or false.',
+    'confirmed' => 'The :attribute field confirmation does not match.',
+    'current_password' => 'The password is incorrect.',
+    'date' => 'The :attribute field is not a valid date.',
+    'date_equals' => 'The :attribute field must be a date equal to :date.',
+    'date_format' => 'The :attribute field must match the format :format.',
+    'decimal' => 'The :attribute field must have :decimal decimal places.',
+    'declined' => 'The :attribute field must be declined.',
+    'declined_if' => 'The :attribute field must be declined when :other is :value.',
+    'different' => 'The :attribute field and :other must be different.',
+    'digits' => 'The :attribute field must be :digits digits.',
+    'digits_between' => 'The :attribute field must be between :min and :max digits.',
+    'dimensions' => 'The :attribute field has invalid image dimensions.',
+    'distinct' => 'The :attribute field has a duplicate value.',
+    'doesnt_end_with' => 'The :attribute field must not end with one of the following: :values.',
+    'doesnt_start_with' => 'The :attribute field must not start with one of the following: :values.',
+    'email' => 'The :attribute field must be a valid email address.',
+    'integer' => 'The :attribute field must be an integer.',
     'max' => [
         'numeric' => 'The :attribute field must not be greater than :max.',
         'file' => 'The :attribute field must not be greater than :max kilobytes.',
         'string' => 'The :attribute field must not be greater than :max characters.',
         'array' => 'The :attribute field must not have more than :max items.',
     ],
+    'min' => [
+        'numeric' => 'The :attribute field must be at least :min.',
+        'file' => 'The :attribute field must be at least :min kilobytes.',
+        'string' => 'The :attribute field must be at least :min characters.',
+        'array' => 'The :attribute field must have at least :min items.',
+    ],
+    'required' => 'The :attribute field is required.',
+    'string' => 'The :attribute field must be a string.',
+    'timezone' => 'The :attribute field must be a valid timezone.',
+    'unique' => 'The :attribute has already been taken.',
+    'uploaded' => 'The :attribute failed to upload.',
+    'uppercase' => 'The :attribute field must be uppercase.',
+    'url' => 'The :attribute field must be a valid URL.',
+    'ulid' => 'The :attribute field must be a valid ULID.',
+    'uuid' => 'The :attribute field must be a valid UUID.',
 
     /*
     |--------------------------------------------------------------------------
@@ -58,6 +93,9 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'email' => 'email address',
+        'password' => 'password',
+    ],
 
 ];
