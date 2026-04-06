ddfb08c000cae245895606730d094e9f0311caff
Roman-PRV <prokopenkorv@gmail.com>
2026-04-06 23:38:37 +0200

    fix: register TrueFalseImageLevel in TtsModelMappingEnum ss-069

--- laravel/tests/Unit/Games/TrueFalseImage/Http/Resources/TrueFalseImageResourceTest.php
+++ laravel/tests/Unit/Games/TrueFalseImage/Http/Resources/TrueFalseImageResourceTest.php
@@ -10,7 +10,6 @@ use Tests\TestCase;
 
 class TrueFalseImageResourceTest extends TestCase
 {
-
     protected function setUp(): void
     {
         parent::setUp();
--- laravel/tests/Unit/Games/TrueFalseText/Http/Resources/TrueFalseTextResourceTest.php
+++ laravel/tests/Unit/Games/TrueFalseText/Http/Resources/TrueFalseTextResourceTest.php
@@ -10,7 +10,6 @@ use Tests\TestCase;
 
 class TrueFalseTextResourceTest extends TestCase
 {
-
     protected function setUp(): void
     {
         parent::setUp();
